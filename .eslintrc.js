c31f5e27aa9617b2458e924b34c0b3053efee68f
DESKTOP-2LKCLDC\thaow <t.haowjern@gmail.com>
2021-08-22 16:17:45 +0800

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: "module",
+	},
+	plugins: [
+		"@typescript-eslint",
+	],
+	rules: {
+		quotes: [
+			"error",
+			"double",
+		],
+		semi: [
+			"error",
+			"always",
+		],
+	},
+	ignorePatterns: ["out", ".next"],
+};
